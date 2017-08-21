c20e8ace1fddd2a2de66ac760f1c6ee7fc409959
uojo <499620917@qq.com>
2017-08-21 16:10:16 +0800

    新增匿名记录

--- src/delayExec.js
+++ src/delayExec.js
@@ -3,18 +3,22 @@ module.exports = function( delayTime = 0 ){
 	let map = {};
 	
 	// 回执，表示 exec_fn 已经结束
-	this.run = ( record )=>{
-		let nowTime = (new Date).getTime()
-		let rlt = false;
-		if( map[record] === undefined ){
-			map[record] = nowTime
-			rlt = true;
-		}else if( (nowTime-map[record])>delayTime ){
-			rlt = true;
-			
+	this.hit = ( record )=>{
+		// 默认匿名记录
+		if(record===undefined){
+			record = 'anonymous'
 		}
-
-		return rlt;
+		let nowTime = (new Date).getTime()
+		let pass = false; // 是否允许通过
+		if( map[record] === undefined ){
+			pass = true;
+			map[record] = nowTime;
+		}else if( (nowTime-map[record])>delayTime ){
+			pass = true;
+			map[record] = nowTime;
+		}
+		
+		return pass;
 	}
 	
 } 
